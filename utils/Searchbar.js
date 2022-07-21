4ec2cf39c0b18dff276de6e533bed6ca83e802ee
Fernando Hickman <fernanhick@gmail.com>
2022-07-21 22:25:33 +0100

    api url set

---
+++ utils/Searchbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Searchbar() {
+    return (
+        <form>
+            <input name='searchBar' type='text' />
+            <button>Search</button>
+        </form>
+    )
+}
+
+export default Searchbar
